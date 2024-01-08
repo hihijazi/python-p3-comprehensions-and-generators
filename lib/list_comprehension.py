3fc84528be9c63616f54553c0cf3197206e991cb
hadil <hihijazi@gmail.com>
2024-01-08 12:12:13 -0500

    save changes

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [n for n in num_list if n % 2 ==0]
+print(return_evens([2,3,4,5,6]))
 
 def make_exclamation(sentence_list):
-    pass
+    return [n + "!" for n in sentence_list]
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
